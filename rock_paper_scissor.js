6ef785226097196c9060b94f33f2372bfc0f0169
Arjun Ambasana <arjun.gajjar23@gmail.com>
2022-04-20 21:38:58 -0400

    2

--- rock_paper_scissor.js
+++ rock_paper_scissor.js
@@ -1,11 +1,14 @@
-const getUserChoice = userInput => {
-  userInput = userInput.toLowerCase();
+// define a function that takes userInput and also verifies it.
+const getUserChoice = userInput => { 
+  userInput = userInput.toLowerCase(); // to make sure inputs are in lowercase.
   if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors' || userInput === 'bomb'){
     return userInput;
   } else {
     console.log('Invalid input');
   }
 } 
+
+//define a function that generates random numbers between 0 and 2 and based on a number, gives aqn output.
 function getComputerChoice(){
   randomNumber = (Math.floor(Math.random() * 3));
   switch(randomNumber){
@@ -23,6 +26,8 @@ function getComputerChoice(){
       break;
   }
 }
+
+//define a function to determine the winner between user and computer. remember, Rock destroys scissors, paper covers rock and scissor cuts paper. 
 
 function determineWinner(userChoice, computerChoice){
   if (userChoice === 'bomb'){
@@ -57,6 +62,7 @@ function determineWinner(userChoice, computerChoice){
   }
 }
 
+//now, since everything is in order, let's play game by declaring a last function that accepts userchoice and computerchoicer and declares winner.
 function playGame(){
   const userChoice = getUserChoice('bomb');
   const computerChoice = getComputerChoice();
@@ -64,8 +70,7 @@ function playGame(){
   console.log(computerChoice);
   console.log(determineWinner(userChoice, computerChoice));
 }
-
-playGame();
+playGame(); //calling the function. Play game!!!
 
 
 
